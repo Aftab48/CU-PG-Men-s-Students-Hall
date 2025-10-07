7ddf6ad3d816e58a9ee94a337f9e3bc221b2b838
Aftab Alam <mdalam4884@gmail.com>
2025-10-07 22:57:14 +0530

    2nd draft

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,35 +1,34 @@
-// lib/appwrite.ts
+//lib/appwrite
 
-import {
-  Account,
-  Avatars,
-  Client,
-  Databases,
-  ID,
-  Query,
-  TablesDB,
-} from "react-native-appwrite";
+import Constants from "expo-constants";
+import { Account, Avatars, Client, Databases, ID, Query, TablesDB } from "react-native-appwrite";
+
+const {
+  EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  EXPO_PUBLIC_APPWRITE_BOARDERS_TABLE_ID,
+  EXPO_PUBLIC_APPWRITE_MANAGERS_TABLE_ID,
+  EXPO_PUBLIC_APPWRITE_EXPENSES_TABLE_ID,
+  EXPO_PUBLIC_APPWRITE_MEALS_TABLE_ID,
+  EXPO_PUBLIC_APPWRITE_STAFF_TABLE_ID,
+  EXPO_PUBLIC_APPWRITE_BUCKET_ID,
+} = Constants.expoConfig.extra;
 
 export const appwriteConfig = {
-  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
+  endpoint: EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   platform: "com.aftab.cupg",
-  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!,
-  // Tables
-  boardersTableId:
-    process.env.EXPO_PUBLIC_APPWRITE_BOARDERS_TABLE_ID!,
-  managersTableId:
-    process.env.EXPO_PUBLIC_APPWRITE_MANAGERS_TABLE_ID!,
-  expensesTableId:
-    process.env.EXPO_PUBLIC_APPWRITE_EXPENSES_TABLE_ID!,
-  mealsTableId: process.env.EXPO_PUBLIC_APPWRITE_MEALS_TABLE_ID!,
-  staffTableId: process.env.EXPO_PUBLIC_APPWRITE_STAFF_TABLE_ID!,
-
-  bucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID!,
+  databaseId: EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  boardersTableId: EXPO_PUBLIC_APPWRITE_BOARDERS_TABLE_ID,
+  managersTableId: EXPO_PUBLIC_APPWRITE_MANAGERS_TABLE_ID,
+  expensesTableId: EXPO_PUBLIC_APPWRITE_EXPENSES_TABLE_ID,
+  mealsTableId: EXPO_PUBLIC_APPWRITE_MEALS_TABLE_ID,
+  staffTableId: EXPO_PUBLIC_APPWRITE_STAFF_TABLE_ID,
+  bucketId: EXPO_PUBLIC_APPWRITE_BUCKET_ID,
 };
 
-export const client = new Client();
-client
+export const client = new Client()
   .setEndpoint(appwriteConfig.endpoint)
   .setProject(appwriteConfig.projectId)
   .setPlatform(appwriteConfig.platform);
@@ -39,6 +38,4 @@ export const databases = new Databases(client);
 export const avatars = new Avatars(client);
 export const tables = new TablesDB(client);
 
-
 export { ID, Query };
-
